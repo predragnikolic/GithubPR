de6088e284acbf32bd65f4b34abc6713824492ed
Predrag <idmpepe@gmail.com>
2020-10-25 19:16:49 +0100

    fix hash links

--- docs/nuxt.config.js
+++ docs/nuxt.config.js
@@ -1,8 +1,15 @@
 import theme from '@nuxt/content-theme-docs'
+const base = '/GithubPR/'
 
 export default theme({
     target: 'static',
     router: {
-        base: '/GithubPR/'
+        base
+    },
+    hooks: {
+        "vue-renderer:ssr:templateParams": function (params) {
+            // fix hash links when using router.base - https://github.com/nuxt/content/issues/376#issuecomment-702193217
+            params.HEAD = params.HEAD.replace(`<base href="${base}">`, "");
+        }
     }
 })
